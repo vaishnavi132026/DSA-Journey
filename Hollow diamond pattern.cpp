38aee46d1de8b677bcf5522cf96ec36d658c7261
vaishnavi132026 <vaishnaviharsh132006@gmail.com>
2026-08-15 17:15:02 +0530

    Add file via upload
    I uploaded the hollow diamond pattern in c++. I have learnt its logic.

---
+++ Hollow diamond pattern.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   int n;
+    cout << "enter n";
+    cin >> n;
+    for(int i=0; i<n; i++) {
+//SPACES
+        for(int j=0; j<n-i-1; j++) {
+            cout << " ";
+        }//stars
+        cout << "*";
+        if(i!=0) {
+            for(int j=0; j<2*i-1; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<i+1; j++) {
+            cout << " ";
+        }
+        cout <<"*";
+        
+        if(i!=(n-2)) {
+        for(int j=0; j<2*(n-2-i)-1;j++) {
+            cout << " ";
+        }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
