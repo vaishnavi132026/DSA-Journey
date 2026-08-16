c9aa2e81e62fd526a896676705aeeaf69ad3683b
vaishnavi132026 <vaishnaviharsh132006@gmail.com>
2026-08-16 16:46:56 +0530

    Add Butterfly pattern via upload

---
+++ Butterfly pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   int n;
+    cout << "enter n: ";
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<i+1; j++) {
+            cout << "*";
+        }
+        for(int j=2*(n-i-1); j>0; j--) {
+            cout << " ";
+        }
+        for(int j=0; j<i+1; j++) {
+            cout << "*";
+        }
+        cout <<endl;
+    }
+    for(int i=0; i<n; i++) {
+        for(int j=n-i; j>0; j--) {
+            cout << "*";
+        }
+        for(int j=0; j<2*i; j++) {
+            cout << " ";
+        }
+        for(int j=n-i;j>0;j--){
+        cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
